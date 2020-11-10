994ea633e62425252ebe495920576ea922274bc5
Nake <Nauryzzz@inbox.ru>
2020-11-10 17:47:22 +0500

    VUE

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="v-main-wrapper">
+        {{title}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "v-main-wrapper",
+        components: {
+
+        },
+        props: {},
+        data() {
+            return {
+                title: "Main wrapper"
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+            
+        },
+        mounted() {
+            console.log("HELLO!");
+        }
+    }
+</script>
+
+<style>
+    .v-main-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
